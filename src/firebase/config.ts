88af7be8ce269d9ed9f3751ccd076b520a82de0b
sayida <sayidafatumah01@gmail.com>
2026-05-07 09:01:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7731652692-34021",
+  "appId": "1:807437062122:web:5387b8e9f4f8c5c3d7ec93",
+  "apiKey": "AIzaSyDWh4sSq0UfFN6IP9dmJ94IQscHlfePAwU",
+  "authDomain": "studio-7731652692-34021.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "807437062122"
+};
